1553c0299d84fa79b2532e4171a9e25ab4788f93
Samuel Goodwin <samuel@roundwallsoftware.com>
2013-10-29 22:01:42 +0100

    Use the prompt instead of questions.

--- index.js
+++ index.js
@@ -7,20 +7,24 @@ var net = require('net'),
 
 var rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+    output: process.stdout
 });
+
+rl.setPrompt('>');
+rl.prompt();
 
 var client = net.connect(options);
 
 client.on('data', function(data){
   console.log(data.toString());
-
-  rl.question("sup?", function(answer){
-    client.write(answer + "\n");
-  });
 });
 
 client.on('end', function(){
   console.log('client disconnect');
   process.exit();
+});
+
+rl.on('line', function(line){
+  client.write(line + "\n");
+  rl.prompt();
 });
